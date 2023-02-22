8b2dd88b139485d8fdde59c155f6594146597a55
Difelice-e <difelice.emanuele@gmail.com>
2023-02-22 12:19:24 +0100

    finished exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+
+class Post {
+  Post(this.title, this.author, this.link);
+
+  final String title;
+  final String author;
+  final String link;
+
+  factory Post.fromData(dynamic data) {
+    final title = data['title'];
+    final author = data['by'];
+    final link = data['url'] ?? "";
+
+    return Post(title, author, link);
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +27,74 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Future<List<Post>> downloadData() async {
+    final response = await http.get(Uri.parse(
+        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"));
+    final List<dynamic> postIds = json.decode(response.body);
+
+    return Future.wait(postIds.take(10).map((id) async {
+      final response = await http.get(Uri.parse(
+          "https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty"));
+      return Post.fromData(json.decode(response.body));
+    }));
+  }
+
+  Widget buildList(List<Post> posts) {
+    return ListView.separated(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          leading: IconButton(
+            onPressed: () {
+              launch(posts[index].link);
+            },
+            icon: Icon(Icons.exit_to_app),
+          ),
+          title: Text(posts[index].title),
+          subtitle: Text("by ${posts[index].author}"),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return Container(height: 1, color: Colors.black12);
+      },
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Color(0xFFff6600),
+          title: Text("Hacker News"),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: FutureBuilder(
+          future: downloadData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+
+              default:
+                if (snapshot.error != null) {
+                  return Center(child: Icon(Icons.error, size: 30));
+                } else {
+                  final List<Post>? posts = snapshot.data;
+                  return buildList(posts!);
+                }
+            }
+          },
+        ));
   }
 }
